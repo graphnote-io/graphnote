e84f0f65d0e30d6bff80d74a81ef3bf427235aa0
Hayden Pennington <haydenpennington@icloud.com>
2022-02-20 13:30:21 -0600

    Start adding input for blocks

--- Shared/DocumentView/DocumentView.swift
+++ Shared/DocumentView/DocumentView.swift
@@ -22,6 +22,7 @@ struct DocumentView: View {
     
     @State private var title = ""
     @State private var workspaceTitle = ""
+    @State private var blockText = ""
     
     var open: Binding<Bool>
     
@@ -68,6 +69,10 @@ struct DocumentView: View {
                             .padding(open.wrappedValue ? .leading : [.leading, .trailing, .top], pad)
                             .padding(open.wrappedValue ? .top : [], pad)
                             .foregroundColor(.primary)
+                    }.frame(width: maxBlockWidth)
+                    HStack {
+                        InputField(lineSpacing: textSpacing, text: $blockText)
+                            .padding(pad)
                     }.frame(width: maxBlockWidth)
                     HStack {
                         Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
---
+++ Shared/DocumentView/InputField.swift
@@ -0,0 +1,34 @@
+//
+//  InputField.swift
+//  Graphnote
+//
+//  Created by Hayden Pennington on 2/20/22.
+//
+
+import SwiftUI
+
+struct InputField: View {
+    let lineSpacing: CGFloat
+    var text: Binding<String>
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            TextEditor(text: text)
+                .onChange(of: text.wrappedValue) { _ in
+                    if !text.wrappedValue.filter({ $0.isNewline }).isEmpty {
+                        print("Found new line character")
+                    }
+                }
+                .font(.body)
+                .lineSpacing(lineSpacing)
+                .foregroundColor(.primary)
+                .fixedSize(horizontal: false, vertical: true)
+                
+            if text.wrappedValue == "" {
+                Text(" Press '/' for commands")
+                    .foregroundColor(.gray)
+            }
+        }
+        
+    }
+}
